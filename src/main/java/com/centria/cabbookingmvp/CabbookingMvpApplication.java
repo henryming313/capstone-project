08a7652174f12bc150c8f227163cb79eaeeb339b
Henryzheng <henryming313@gmail.com>
2026-03-05 01:56:59 +0800

    add spring boot cab booking project

---
+++ src/main/java/com/centria/cabbookingmvp/CabbookingMvpApplication.java
@@ -0,0 +1,13 @@
+package com.centria.cabbookingmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabbookingMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabbookingMvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/centria/cabbooking_capstone/project/CabbookingCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centria.cabbooking_capstone.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabbookingCapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
